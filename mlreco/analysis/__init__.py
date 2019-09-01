f66eab9f281d01af2526637a4d9e77106f8f526c
kvtsang <kvtang@gmail.com>
2019-08-31 17:18:28 -0700

    Add my own ghostnet_uresnet analyzer

--- mlreco/analysis/__init__.py
+++ mlreco/analysis/__init__.py
@@ -2,3 +2,10 @@ from mlreco.analysis.track_clustering import track_clustering
 from mlreco.analysis.michel_reconstruction import michel_reconstruction
 from mlreco.analysis.deghosting_metrics import deghosting_metrics
 from mlreco.analysis.instance_clustering import instance_clustering
+
+# -------------------------------
+# custom analysis code by kvtsang
+# -------------------------------
+from mlreco.extras.analysis import \
+        analyze_ghostnet_uresnet
+# -------------------------------
